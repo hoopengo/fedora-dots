1d27ffd38118af59d50fb2e28ffb699522d72cf5
Aleksey <hoopengo@gmail.com>
2022-05-05 15:51:23 -0400

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,5 +1,8 @@
 sudo dnf update -y
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings
+
+lspci | grep VGA
+
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig
 
 sudo dnf update -y
 
