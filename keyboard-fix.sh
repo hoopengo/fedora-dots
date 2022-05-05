c4a58e2db5a4e91570847019bc7ff1a8dd1708c0
Aleksey <hoopengo@gmail.com>
2022-05-05 16:01:12 -0400

    Create keyboard-fix.sh

---
+++ keyboard-fix.sh
@@ -0,0 +1,2 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source \[\'\<Alt\>Shift_L\'\]
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward \[\'\<Shift\>Alt_L\'\]
