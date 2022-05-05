f01f10174fd5e876224df8785255619a011a0119
Aleksey <hoopengo@gmail.com>
2022-05-05 15:07:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ sudo yum update
 sudo dnf upgrade --refresh -y
 sudo dnf makecache --refresh
 
-neofetch
-
 # install my most pkgs
-sudo dnf -y install fish z git curl gcc clang cmake python3 python3-pip fonts-powerline thunderbird wget gnome-tweaks dnf-plugins-core docker-ce docker-ce-cli containerd.io docker-compose-plugin blueman
+sudo dnf -y install fish z git curl gcc clang cmake python3 python3-pip fonts-powerline thunderbird wget gnome-tweaks dnf-plugins-core docker-ce docker-ce-cli containerd.io docker-compose-plugin blueman neofetch
+
+neofetch
 
 # enable docker
 sudo systemctl start docker
