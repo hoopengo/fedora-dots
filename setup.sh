69334b810762ac9e32d27e007f26ee7f3db8f252
Aleksey <hoopengo@gmail.com>
2022-05-05 15:09:46 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo dnf upgrade --refresh -y
 sudo dnf makecache --refresh
 
 # install my most pkgs
-sudo dnf -y install fish z git curl gcc clang cmake python3 python3-pip fonts-powerline thunderbird wget gnome-tweaks dnf-plugins-core docker-ce docker-ce-cli containerd.io docker-compose-plugin blueman neofetch
+sudo dnf -y install fish z git curl gcc clang cmake python3 python3-pip fonts-powerline thunderbird wget gnome-tweaks dnf-plugins-core blueman neofetch
 
 neofetch
 
