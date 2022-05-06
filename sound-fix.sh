c9f4bb660bb3bc3158af78786ea71e341b7f1959
Aleksey <hoopengo@gmail.com>
2022-05-06 13:14:14 +0300

    Update sound-fix.sh

--- sound-fix.sh
+++ sound-fix.sh
@@ -1,1 +1,3 @@
 gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
+gsettings get org.gnome.settings-daemon.plugins.media-keys volume-step
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-step 2
