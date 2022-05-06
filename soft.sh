fb1bc63eff3631c7d3c8af899f01bcb305211618
Aleksey <hoopengo@gmail.com>
2022-05-06 21:58:43 +0300

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -1,4 +1,6 @@
 flatpak update
 flatpak install -y com.mattjakeman.ExtensionManager com.github.unrud.VideoDownloader com.obsproject.Studio de.haeckerfelix.Fragments dev.geopjr.Collision fr.handbrake.ghb fr.romainvigier.MetadataCleaner io.github.arunsivaramanneo.GPUViewer io.mpv.Mpv org.gnome.DejaDup org.kde.kdenlive org.kde.krita org.telegram.desktop org.onlyoffice.desktopeditors com.discordapp.Discord com.google.Chrome
 
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap refresh
 sudo snap install --classic code pycharm-community
