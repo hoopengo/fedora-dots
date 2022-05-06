281a61e13fb7f986945c69d81f5b9f05ab5c3d81
Aleksey <hoopengo@gmail.com>
2022-05-06 14:21:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,35 @@
-# init
+import os
+
+def prompt(q: str) -> bool:
+  match input(f'{q} (Y/n): ').lower():
+    case ["yes", "y", ""]:
+      print('Начинаем исполнение.')
+      return True
+    case _:
+      print('Пропускаем этот пункт.')
+      return False
+
+  return False
+
+if prompt('Выполнить первичную установку?'):
+  os.system('sh setup.sh')
+  if prompt('Перезагрузить устройство? Это поможет в устанении многих ошибок'):
+    os.system('sudo reboot')
+else:
+  os.system('dnf update')
+
+if prompt('Начать фиксить звук?'):
+    os.system('sh alert-sound-fix.sh')
+    os.system('sh sound-fix.sh')
+
+if prompt('Пофиксить смену раскладки по ALT+SHIFT?'):
+    os.system('sh keyboard-fix.sh')
+
+if prompt('Установить драйвера NVIDIA? После, система сразу перезапустится'):
+  os.system('sh nvidia.sh')
+  
+if prompt('Установить софт для игр?'):
+  os.system('sh games.sh')
+
+if prompt('Установить весь софт?'):
+  os.system('sh soft.sh')
