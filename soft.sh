6e9003eb64b939a702737f81425b4611233290aa
Aleksey <hoopengo@gmail.com>
2022-05-06 21:54:45 +0300

    Update soft.sh

--- soft.sh
+++ soft.sh
@@ -1,2 +1,4 @@
 flatpak update
-flatpak install -y com.mattjakeman.ExtensionManager com.github.unrud.VideoDownloader com.obsproject.Studio de.haeckerfelix.Fragments dev.geopjr.Collision fr.handbrake.ghb fr.romainvigier.MetadataCleaner io.github.arunsivaramanneo.GPUViewer io.mpv.Mpv org.gnome.DejaDup org.kde.kdenlive org.kde.krita org.telegram.desktop org.onlyoffice.desktopeditors com.discordapp.Discord flathub com.visualstudio.code flathub com.jetbrains.PyCharm-Community flathub com.google.Chrome
+flatpak install -y com.mattjakeman.ExtensionManager com.github.unrud.VideoDownloader com.obsproject.Studio de.haeckerfelix.Fragments dev.geopjr.Collision fr.handbrake.ghb fr.romainvigier.MetadataCleaner io.github.arunsivaramanneo.GPUViewer io.mpv.Mpv org.gnome.DejaDup org.kde.kdenlive org.kde.krita org.telegram.desktop org.onlyoffice.desktopeditors com.discordapp.Discord com.google.Chrome
+
+sudo snap install --classic code pycharm-community
