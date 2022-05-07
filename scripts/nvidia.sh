77d911cd399f35f55f62d196cbb673ed3531f9d6
Aleksey <hoopengo@gmail.com>
2022-05-08 02:36:45 +0300

    Update nvidia.sh

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -1,8 +1,9 @@
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf update -y
 
 lspci | grep VGA
 
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig vulkan-tools vulkan-loader-devel vulkan-validation-layers-devel glfw-devel sudo dnf install glm-devel
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig vulkan vulkan-tools vulkan-loader-devel vulkan-validation-layers-devel glfw-devel glm-devel vkBasalt
 
 sudo dnf update -y
 
