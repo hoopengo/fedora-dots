35c4ec695c69b2b99b934c95a384fed496f206d9
Aleksey <hoopengo@gmail.com>
2022-05-07 23:07:27 +0300

    Update games.sh

--- scripts/games.sh
+++ scripts/games.sh
@@ -1,6 +1,6 @@
 sudo dnf update
 flatpak update
 
-flatpak install flathub com.valvesoftware.Steam com.heroicgameslauncher.hgl flathub com.github.tchx84.Flatseal flathub net.davidotek.pupgui2
+flatpak install flathub com.valvesoftware.Steam flathub com.github.tchx84.Flatseal flathub net.davidotek.pupgui2
 
 sudo dnf install lutris steam-devices
