bc5e3dbd26d3b587d9d2d0ec80c8511d3e56c65e
Aleksey <hoopengo@gmail.com>
2022-05-08 13:28:24 +0300

    Update nvidia.sh

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -3,7 +3,7 @@ sudo dnf update -y
 
 lspci | grep VGA
 
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig vulkan vulkan-tools vulkan-loader-devel vulkan-validation-layers-devel glfw-devel glm-devel vkBasalt
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power xorg-x11-drv-nvidia-cuda nvidia-settings gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig vulkan vulkan-tools vulkan-loader-devel vulkan-validation-layers-devel glfw-devel glm-devel vkBasalt
 
 sudo dnf update -y
 
