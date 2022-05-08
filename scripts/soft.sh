02fdad86ae2dee9955d084782d1fd8b582dd1568
Aleksey <hoopengo@gmail.com>
2022-05-08 13:46:47 +0300

    Update soft.sh

--- scripts/soft.sh
+++ scripts/soft.sh
@@ -1,5 +1,5 @@
 flatpak update
-flatpak install -y com.mattjakeman.ExtensionManager com.github.unrud.VideoDownloader com.obsproject.Studio de.haeckerfelix.Fragments dev.geopjr.Collision fr.handbrake.ghb fr.romainvigier.MetadataCleaner io.github.arunsivaramanneo.GPUViewer io.mpv.Mpv org.gnome.DejaDup org.kde.kdenlive org.kde.krita org.telegram.desktop org.onlyoffice.desktopeditors com.discordapp.Discord com.google.Chrome
+flatpak install -y com.mattjakeman.ExtensionManager com.github.unrud.VideoDownloader com.obsproject.Studio de.haeckerfelix.Fragments dev.geopjr.Collision fr.handbrake.ghb fr.romainvigier.MetadataCleaner io.github.arunsivaramanneo.GPUViewer io.mpv.Mpv org.gnome.DejaDup org.kde.kdenlive org.kde.krita org.onlyoffice.desktopeditors
 
 sudo ln -s /var/lib/snapd/snap /snap
 sudo snap refresh
